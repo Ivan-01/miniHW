646ff1d6a9b19f3c541b1dd8f45d6000f3581bdd
ititkov <ititkov@42.us.org>
2019-09-10 21:25:42 -0700

    Opentrons

--- protocol.py
+++ protocol.py
@@ -1,13 +1,13 @@
-
 from opentrons import robot, instruments, containers
 from opentrons.util.vector import Vector
 from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
+from sys import exit
 
 robot.connect(robot.get_serial_ports_list()[0])
 robot.home()
-#robot.head_speed (4000)
+robot.head_speed (20000)
 
 # Define map
 
@@ -39,14 +39,14 @@ pipette = instruments.Pipette(
 		min_volume=0,
 		max_volume=200,
 		aspirate_speed = 300,
-		dispense_speed = 2000,
+		dispense_speed = 1000,
 		tip_racks = rack[0],
 		trash_container = trash[0]
 )
 # Define Custom Protocol
-i = 0
-rad = 43.2
-num = 20
+i = 0 
+rad = 41.5
+num = 96 
 t = np.random.uniform(0.0, 2.0*np.pi, num)
 r = rad * np.sqrt(np.random.uniform(0.0, 1.0, num))
 x = r * np.cos(t)
@@ -54,12 +54,30 @@ y = r*np.sin(t)
 
 while i < 3:
 	pipette.pick_up_tip(rack.wells(i))
-	for x1, y1 in x, y:
-		pipette.move_to((water[i], Vector(0, 0, 30))).aspirate(30)
-		pipette.aspirate(50, water[i])
-		pipette.move_to((dish[i], Vector(x1, y1, 0))).aspirate(50)
-#		pipette.aspirate(50, dish[i])
-		pipette.dispense(200, samples[i].well(j)).blow_out()
+	j = 0
+	for x1, y1 in zip(x, y):
+
+		pipette.move_to(( water[i], Vector( 0, 0, 30 ) ), 'arc' ).aspirate( 20 )
+		pipette.aspirate( 50 , water[i] )
+		pipette.move_to(( water[i], Vector( 0, 0, 30 ) ), 'arc' ).aspirate( 20 )
+		pipette.aspirate( 50 , water[i] )
+
+#		pipette.move_to( (dish[i], Vector(0,0,0))).aspirate(50)
+#		pipette.move_to( (dish[i], Vector(42,0,0)))
+#		pipette.move_to( (dish[i], Vector(-42,0,0)))
+#		pipette.move_to( (dish[i], Vector(0,42,0)))
+#		pipette.move_to( (dish[i], Vector(0,-42,0)))
+		pipette.move_to( ( dish[i], Vector(x1, y1, 0))).aspirate(60)
+
+		pipette.move_to( ( dish[i], Vector( x1 + 2 , y1       , 0 ) ), 'direct' )
+		pipette.move_to( ( dish[i], Vector( x1 - 2 , y1       , 0 ) ), 'direct' )
+		pipette.move_to( ( dish[i], Vector( x1       , y1       , 0 ) ), 'direct' )
+		pipette.move_to( ( dish[i], Vector( x1       , y1 + 2 , 0 ) ), 'direct' )
+		pipette.move_to( ( dish[i], Vector( x1       , y1 - 2 , 0 ) ), 'direct' )
+
+		pipette.dispense( 500, samples[i].well(j)).blow_out()
+		j += 1
+
 	pipette.drop_tip(trash)
 	i = i + 1
 
